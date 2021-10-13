0c3bb5a994abac12c13c2f617e5b2c0c236e3f15
LongVu80 <thienlong14th@gmail.com>
2021-10-12 20:46:16 -0500

    update cookie, header, footer, javaScript, css

--- js/test.js
+++ js/test.js
@@ -1,3 +1,17 @@
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieButton = document.querySelector(".cookie-btn");
+
+cookieButton.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("cookieBannerDisplayed", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("cookieBannerDisplayed")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
+
 $(document).ready(function() {
 	$.get('https://fakestoreapi.com/products', function (data) {
         const filters = {
@@ -300,3 +314,22 @@ $(document).ready(function() {
   displayCart();
     });
 });
+
+var apiKey = '6b71e45e81339efb68c7b8d995519887'
+
+
+    $('form').submit(function () {
+        var city = $('#city').val();
+        var cityString = `${city}`
+
+        var url = `https://api.openweathermap.org/data/2.5/weather?q=${cityString}&appid=${apiKey}&units=imperial`
+
+        $.get(url, function (res) {
+            console.log(res)
+            var htmlString = `<h1>${cityString}</h1><p>Temperature: ${res.main.temp}&#8457;</p>`
+            htmlString+= `<p>Description: ${res.weather[0].description}</p><p>Wind Speed: ${res.wind.speed} mph</p>`
+            $('#forecast').html(htmlString);
+
+        }, 'json');
+        return false;
+    });
